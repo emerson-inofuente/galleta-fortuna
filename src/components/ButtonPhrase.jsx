2461461ad091ffc167adc832c66f771a67eeaf01
Emerson <emerson.inofuente@hotmail.com>
2024-01-22 22:05:25 -0500

    first commit

--- src/components/ButtonPhrase.jsx
+++ src/components/ButtonPhrase.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import phrases from '../utils/phrases.json';
-import randomIndex from '../services/randomIndex';
+import randomIndex from '../services/randomindex.js';
 import bgArray from "../utils/bgArray.json";
 
 const ButtonPhrase = ({setQuote, setBgApp}) => {
 
     const handleButton = () => {
         const bgIndex = randomIndex (bgArray);
-        const sentence = randomIndex(phrases);
+        const sentence = randomIndex (phrases);
         setQuote(sentence);
         setBgApp(bgIndex);
     }
